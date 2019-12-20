0e6a392386cfa42e7bfa2ac1bbcb42acf9d80bac
Baojian Zhou <bzhou6@albany.edu>
2019-12-20 18:14:17 -0500

    dd

--- sparse_learn/__init__.py
+++ sparse_learn/__init__.py
@@ -1,6 +1,7 @@
 name = "sparse_learn"
 from sparse_learn import c
 from sparse_learn import base
+from sparse_learn import examples
 from sparse_learn import graph_utils
 from sparse_learn import data_process
 from sparse_learn import algo_graph_da
---
+++ sparse_learn/examples/__init__.py
@@ -0,0 +1,6 @@
+name = "examples"
+from sparse_learn.examples import example_model
+from sparse_learn.examples import example_test_all
+from sparse_learn.examples import example_test_proj
+from sparse_learn.examples import example_graph_da
+from sparse_learn.examples import example_graph_sto_iht
